d71b80a0424847703f3151d314b93279a56312a8
henry-25 <henry.r.welsh@gmail.com>
2020-02-24 14:27:54 -0500

    Correctly finding the number of lights on

--- 18/gif_lights.py
+++ 18/gif_lights.py
@@ -1,45 +1,86 @@
 import pprint
+import copy
 
-# Part 1
-
-
-def get_neighbors(curr_x, curr_y, lg):
-    num_neighbors = 0
-    # Top left
-    if curr_x > 0 and curr_y > 0 and lg[curr_y - 1][curr_x - 1] == '#':
-        num_neighbors += 1
-    # Top middle
-    if curr_y > 0 and lg[curr_y - 1][curr_x] == '#':
-        num_neighbors += 1
-    # Top right
-    if curr_x < len(lg) and curr_y > 0 and lg[curr_y - 1][curr_x + 1] == '#':
-        num_neighbors += 1
-    # Middle right
-    if curr_x < len(lg) and lg[curr_y][curr_x + 1] == '#':
-        num_neighbors += 1
-    # Bottom right
-    if curr_x < len(lg) and curr_y < len(lg) and lg[curr_y + 1][curr_x + 1] == '#':
-        num_neighbors += 1
-    # Bottom middle
-    if curr_y < len(lg) and lg[curr_y + 1][curr_x] == '#':
-        num_neighbors += 1
-    # Bottom left
-    if curr_x > 0 and curr_y < len(lg) and lg[curr_y + 1][curr_x - 1] == '#':
-        num_neighbors += 1
-    # Middle left
-    if curr_x > 0 and lg[curr_y][curr_x - 1] == '#':
-        num_neighbors += 1
-    return num_neighbors
-
-
-lights_grid = [['.' for i in range(0, 100)] for j in range(0, 100)]
+lights_grid = []
 
 with open('input.txt') as file:
-    for n, line in enumerate(file):
+    for line_num, line in enumerate(file):
         line = line.strip()
-        for t, ch in enumerate(line):
-            lights_grid[n][t] = ch
+        lights_grid.append([])
+        for col_num, ch in enumerate(line):
+            lights_grid[line_num].append(ch)
 
-tmp_grid = lights_grid[:]
 
-print(len(tmp_grid))
+def check_adjacent(line_number, column_number, lights):
+    lights_on = 0
+    if line_number > 0:
+        if column_number > 0:
+            # Top Left
+            if lights[line_number - 1][column_number - 1] == '#':
+                lights_on += 1
+        if column_number < len(lights_grid) - 1:
+            # Top Right
+            if lights[line_number - 1][column_number + 1] == '#':
+                lights_on += 1
+        # Middle Top
+        if lights[line_number - 1][column_number] == '#':
+            lights_on += 1
+    if line_number < len(lights_grid) - 1:
+        if column_number < len(lights_grid) - 1:
+            # Bottom Right
+            if lights[line_number + 1][column_number + 1] == '#':
+                lights_on += 1
+        if column_number > 0:
+            # Bottom Left
+            if lights[line_number + 1][column_number - 1] == '#':
+                lights_on += 1
+        # Middle Bottom
+        if lights[line_number + 1][column_number] == '#':
+            lights_on += 1
+    if column_number > 0:
+        # Middle Left
+        if lights[line_number][column_number - 1] == '#':
+            lights_on += 1
+    if column_number < len(lights_grid) - 1:
+        # Middle Right
+        if lights[line_number][column_number + 1] == '#':
+            lights_on += 1
+    return lights_on
+
+
+def take_step(initial_state):
+    tmp_state = copy.deepcopy(initial_state)
+    for l_num, l in enumerate(tmp_state):
+        for c_num, c in enumerate(l):
+            adjacent_lights = check_adjacent(l_num, c_num, initial_state)
+            corner_spots = [(0, 0), (0, len(l) - 1), (len(l) - 1, 0), (len(l) - 1, len(l) - 1)]
+            if (l_num, c_num) in corner_spots:
+                tmp_state[l_num][c_num] = '#'
+            elif c == '#':
+                if adjacent_lights == 2 or adjacent_lights == 3:
+                    tmp_state[l_num][c_num] = '#'
+                else:
+                    tmp_state[l_num][c_num] = '.'
+            else:
+                if adjacent_lights == 3:
+                    tmp_state[l_num][c_num] = '#'
+                else:
+                    tmp_state[l_num][c_num] = '.'
+    return tmp_state
+
+
+# Part 1
+# Part 2 input changed to make corner spots all lights on
+step_num = 0
+while step_num < 100:
+    lights_grid = take_step(lights_grid)
+    step_num += 1
+
+lights_on = 0
+for i in lights_grid:
+    for j in i:
+        if j == '#':
+            lights_on += 1
+
+pprint.pprint(lights_grid)
+print(lights_on)
